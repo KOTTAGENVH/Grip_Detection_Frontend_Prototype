36880a6e994346c87b8c5d7458d686c1deec07ee
Kottage N.V.H <nowendevelopment@gmail.com>
2024-05-01 18:39:37 +0530

    feat:  added home page

--- frontend/src/app/Home/page.tsx
+++ frontend/src/app/Home/page.tsx
@@ -1,66 +1,49 @@
 "use client";
 import Header from "@/components/header";
-import Webcam from "react-webcam";
-import React, { useRef, useEffect, useState } from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import React, { useRef, useEffect, useState, use } from "react";
+import { styled } from "@mui/material/styles";
+import Link from 'next/link';
+
+// Styled Button with Glass Effect
+const GlassButton = styled(Button)(({ theme }) => ({
+  "&.MuiButton-contained": {
+    transition: "background-color 0.3s ease",
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
+    backdropFilter: "blur(8px)",
+    border: "1px solid transparent",
+    "&:hover": {
+      backgroundColor: "rgba(255, 255, 255, 0.8)",
+      color: "black",
+    },
+  },
+}));
 
 function HomePage() {
-  const [selectedCamera, setSelectedCamera] = useState<string | null>(null);
-  const [cameras, setCameras] = useState<MediaDeviceInfo[]>([]);
-  const webcamRef = useRef<Webcam>(null);
   let title: string = "Home";
-
-  useEffect(() => {
-    const fetchCamera = async () => {
-      // Fetch available cameras
-      const availableCameras = await navigator.mediaDevices.enumerateDevices();
-      const videoDevices = availableCameras.filter(
-        (device) => device.kind === "videoinput"
-      );
-      setCameras(videoDevices);
-      setSelectedCamera(videoDevices[0]?.deviceId || null);
-    };
-
-    fetchCamera();
-  }, []);
-
-  // Function to handle camera change
-  const handleCameraChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedCamera(event.target.value);
-  };
 
   return (
     <div>
       <Header title={title} />
-      <select
-        aria-label="Select Camera"
-        value={selectedCamera || ""}
-        onChange={handleCameraChange}
+      <div
         style={{
-          backgroundColor: "white",
-          color: "black",
-          padding: "10px",
-          borderRadius: "10px",
-          margin: "10px",
-          position: "absolute",
-          zIndex: 10,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "90vh",
+          width: "100vw",
         }}
       >
-        {cameras.map((camera) => (
-          <option key={camera.deviceId} value={camera.deviceId}>
-            {camera.label || `Camera ${cameras.indexOf(camera) + 1}`}
-          </option>
-        ))}
-      </select>
-      <Webcam
-        ref={webcamRef}
-        videoConstraints={selectedCamera ? { deviceId: selectedCamera } : {}}
-        style={{
-          left: 0,
-          top: 0,
-          width: "100vw",
-          height: "90vh",
-        }}
-      />
+      <Stack spacing={2} direction="row">
+          <Link href="/camera">
+            <GlassButton variant="contained">Camera</GlassButton>
+          </Link>
+          <Link href="/photo_uploader">
+            <GlassButton variant="contained">Uploader</GlassButton>
+          </Link>
+        </Stack>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/app/camera/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+import Header from "@/components/header";
+import Webcam from "react-webcam";
+import React, { useRef, useEffect, useState } from "react";
+function Camera() {
+  const [selectedCamera, setSelectedCamera] = useState<string | null>(null);
+  const [cameras, setCameras] = useState<MediaDeviceInfo[]>([]);
+  const webcamRef = useRef<Webcam>(null);
+  let title: string = "Camera";
+
+  useEffect(() => {
+    const fetchCamera = async () => {
+      // Fetch available cameras
+      const availableCameras = await navigator.mediaDevices.enumerateDevices();
+      const videoDevices = availableCameras.filter(
+        (device) => device.kind === "videoinput"
+      );
+      setCameras(videoDevices);
+      setSelectedCamera(videoDevices[0]?.deviceId || null);
+    };
+
+    fetchCamera();
+  }, []);
+
+  // Function to handle camera change
+  const handleCameraChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedCamera(event.target.value);
+  };
+
+  return (
+    <div>
+      <Header title={title} />
+      <select
+        aria-label="Select Camera"
+        value={selectedCamera || ""}
+        onChange={handleCameraChange}
+        style={{
+          backgroundColor: "white",
+          color: "black",
+          padding: "10px",
+          borderRadius: "10px",
+          margin: "10px",
+          position: "absolute",
+          zIndex: 10,
+        }}
+      >
+        {cameras.map((camera) => (
+          <option key={camera.deviceId} value={camera.deviceId}>
+            {camera.label || `Camera ${cameras.indexOf(camera) + 1}`}
+          </option>
+        ))}
+      </select>
+      <Webcam
+        ref={webcamRef}
+        videoConstraints={selectedCamera ? { deviceId: selectedCamera } : {}}
+        style={{
+          left: 0,
+          top: 0,
+          width: "100vw",
+          height: "90vh",
+        }}
+      />
+    </div>
+  );
+}
+
+export default Camera;
